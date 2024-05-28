04e8257a6b3722309de57378bb00cea5827fe48b
Nilesh Laad <quic_nlaad@quicinc.com>
2024-05-28 13:26:09 +0530

    disp: msm: dp: parse display_type before connector initialization
    dp_parser is only available post connector init, so if parser is
    not available get display_type from devicetree itself and remove
    redundant code.
    
    Signed-off-by: Nilesh Laad <quic_nlaad@quicinc.com>
    Change-Id: I2131a257cb4795f52be1706882dce533baf94600

--- msm/dp/dp_display.c
+++ msm/dp/dp_display.c
@@ -3855,6 +3855,7 @@ static int dp_display_get_display_type(struct dp_display *dp_display,
 		const char **display_type)
 {
 	struct dp_display_private *dp;
+	struct device_node *of_node;
 
 	if (!dp_display || !display_type) {
 		pr_err("invalid input\n");
@@ -3865,7 +3866,11 @@ static int dp_display_get_display_type(struct dp_display *dp_display,
 
 	if (dp->parser)
 		*display_type = dp->parser->display_type;
-
+	else {
+		of_node = dp->pdev->dev.of_node;
+		*display_type = of_get_property(of_node, "qcom,display-type",
+					NULL);
+	}
 	return 0;
 }
 
